c9b4b1c0d7d7774a0de63cd8a5cd33f8fd28b93f
Rhys Mahannah <rhys.mahannah@gmail.com>
2024-09-09 12:04:57 -0700

    refactor: add express static for js, css, html

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.listen(port, () => {
-    console.log(`Dummy app listening on port ${port}`)
+    console.log(`Server is running on http://localhost:${port}`)
 });
